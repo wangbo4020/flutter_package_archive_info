264ee3a0b83c2783cc327b47669cb815e4d09011
wangxing <wangxing@adups.com>
2020-09-14 14:05:09 +0800

    Add code comment

--- lib/package_archive_info.dart
+++ lib/package_archive_info.dart
@@ -3,8 +3,20 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:package_info/package_info.dart';
 
+/// Application metadata. Provides .apk package information on Android.
+/// Non-support .ipa
+///
+/// ```dart
+/// PackageInfo packageInfo = await PackageArchiveInfo.fromPath('File path.apk')
+/// print("Version is: ${packageInfo.version}");
+/// ```
 class PackageArchiveInfo extends PackageInfo {
-
+  /// Constructs an instance with the given values for testing. [PackageArchiveInfo]
+  /// instances constructed this way won't actually reflect any real information
+  /// from the platform, just whatever was passed in at construction time.
+  ///
+  /// See [fromPath] for the right API to get a [PackageArchiveInfo] that's
+  /// actually populated with real data.
   PackageArchiveInfo({
     String appName,
     String packageName,
@@ -20,6 +32,7 @@ class PackageArchiveInfo extends PackageInfo {
   static const MethodChannel _channel =
       const MethodChannel('com.wangbo4020/package_archive_info');
 
+  /// Retrieves package information from the local file.
   static Future<PackageArchiveInfo> fromPath(String path) {
     return _channel.invokeMapMethod('getPackageArchiveInfo', {
       "archiveFilePath": path,
